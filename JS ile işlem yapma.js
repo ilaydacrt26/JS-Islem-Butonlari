7f34cedfc36025b357bddd09ae75d97fe443fe04
ilaydacrt26 <ilayda.cirit26@gmail.com>
2023-02-18 16:19:25 +0300

    First Commit

---
+++ JS ile işlem yapma.js
@@ -0,0 +1,22 @@
+let a = 125;
+let b = 5;
+
+function topla() {
+    alert("Toplam: " + (a + b));
+}
+
+function çıkar() {
+    alert("Fark: " + (a - b));
+}
+
+function çarp() {
+    alert("Çarpım: " + (a * b));
+}
+
+function böl() {
+    alert("Bölüm: " + (a / b));
+}
+
+function mod() {
+    alert("Mod: " + (a % b));
+}
